e8848b020f683667884928def73b488352bbd625
Carolina Cruz <caro21_cruz@hotmail.com>
2023-01-17 04:54:21 -0600

    Se agregaron comentarios acarpeta Utils

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,7 @@
 module.exports = {
+    //Método para dar formato a la fecha
     format_date: date => {
+        //Crear fecha y regresar valor
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
     },
 };
